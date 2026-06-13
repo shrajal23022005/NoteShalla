3ba899ff2c4252e50203664651e4a53efe087df9
shrajal23022005 <shrajal2302@gmail.com>
2026-06-13 09:01:02 +0530

    Final NoteShalla deployment setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -92,7 +92,13 @@ const login = async (req, res) => {
       { expiresIn: '7d' }
     );
 
-    res.cookie('token', token, cookieOptions);
+    // res.cookie('token', token, cookieOptions);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    });
 
     return res.json({
       message: 'Logged in successfully',
